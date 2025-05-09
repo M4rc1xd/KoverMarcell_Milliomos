ab2234084ea4a2b7d808569c1d3e58e3a8bf5df7
Kover Marcell <1228marci@gmail.com>
2025-05-09 12:42:08 +0200

    keszenvan a sima kerdes sor beolvasasa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Milliomos
 {
@@ -6,7 +8,23 @@ namespace Milliomos
     {
         static void Main(string[] args)
         {
-            
+            List<Kerdes> kerdesek = new List<Kerdes>();
+            string fajlnev = "kerdesek.txt";
+            Beolvasas(fajlnev, kerdesek);
+            for (int i = 0; i < kerdesek.Count; i++)
+            {
+                Console.WriteLine($"{kerdesek[i]}");
+            }
+        }
+
+        static void Beolvasas(string fajlnev, List<Kerdes> kerdesek)
+        {
+            StreamReader sr = new(fajlnev);
+            while(!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                kerdesek.Add(new Kerdes(int.Parse(sor[0]), sor[1], sor[2], sor[3], sor[4], sor[5], sor[6], sor[7]));
+            }
         }
     }
 }
