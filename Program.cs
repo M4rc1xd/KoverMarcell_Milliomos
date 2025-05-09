82f21bfa19d375c8409c97b1fcd38cd779ab2f72
Kover Marcell <1228marci@gmail.com>
2025-05-09 12:53:38 +0200

    sorkerdesek beolvasasa kesz

--- Program.cs
+++ Program.cs
@@ -9,21 +9,33 @@ namespace Milliomos
         static void Main(string[] args)
         {
             List<Kerdes> kerdesek = new List<Kerdes>();
+            List<Sorkerdes> sorkerdesek = new List<Sorkerdes>();
             string fajlnev = "kerdesek.txt";
-            Beolvasas(fajlnev, kerdesek);
-            for (int i = 0; i < kerdesek.Count; i++)
+            KerdesBeolvasas(fajlnev, kerdesek);
+            string fajlnev2 = "sorkerdesek.txt";
+            SorkerdesBeolvasas(fajlnev2, sorkerdesek);
+            for (int i = 0; i < sorkerdesek.Count; i++)
             {
-                Console.WriteLine($"{kerdesek[i]}");
+                Console.WriteLine($"{sorkerdesek[i]}");
             }
         }
 
-        static void Beolvasas(string fajlnev, List<Kerdes> kerdesek)
+        static void KerdesBeolvasas(string fajlnev, List<Kerdes> kerdesek)
         {
             StreamReader sr = new(fajlnev);
             while(!sr.EndOfStream)
             {
                 string[] sor = sr.ReadLine().Split(';');
                 kerdesek.Add(new Kerdes(int.Parse(sor[0]), sor[1], sor[2], sor[3], sor[4], sor[5], sor[6], sor[7]));
+            }
+        }
+        static void SorkerdesBeolvasas(string fajlnev, List<Sorkerdes> sorkerdesek)
+        {
+            StreamReader sr = new(fajlnev);
+            while(!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                sorkerdesek.Add(new Sorkerdes(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5], sor[6]));
             }
         }
     }
--- sorkerdes.cs
+++ sorkerdes.cs
@@ -1,7 +1,26 @@
 namespace Milliomos
 {
-    public class sorkerdes
+    public class Sorkerdes
     {
-        
+        public string kerdes;
+        public string valasz1;
+        public string valasz2;
+        public string valasz3;
+        public string valasz4;
+        public string helyesValasz;
+        public string kategoria;
+        public Sorkerdes(string kerdes, string valasz1, string valasz2, string valasz3, string valasz4, string helyesValasz, string kategoria)
+        {
+            this.kerdes = kerdes;
+            this.valasz1 = valasz1;
+            this.valasz2 = valasz2;
+            this.valasz3 = valasz3;
+            this.valasz4 = valasz4;
+            this.helyesValasz = helyesValasz;
+        }
+        public override string ToString()
+        {
+            return $"{kerdes}: A:{valasz1} B:{valasz2} C:{valasz3} D:{valasz4}";
+        }
     }
 }
