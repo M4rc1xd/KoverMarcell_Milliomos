61ba0cb13bd35921af43700788fd980748518253
Kover Marcell <1228marci@gmail.com>
2025-05-09 12:56:40 +0200

    ora vegi push

--- kerdes.cs
+++ kerdes.cs
@@ -4,20 +4,14 @@ namespace Milliomos
     {
         public int nehezseg;
         public string kerdes;
-        public string valasz1;
-        public string valasz2;
-        public string valasz3;
-        public string valasz4;
+        public List<string> valaszok;
         public string helyesValasz;
         public string kategoria;
         public Kerdes(int nehezseg, string kerdes, string valasz1, string valasz2, string valasz3, string valasz4, string helyesValasz, string kategoria)
         {
             this.nehezseg = nehezseg;
             this.kerdes = kerdes;
-            this.valasz1 = valasz1;
-            this.valasz2 = valasz2;
-            this.valasz3 = valasz3;
-            this.valasz4 = valasz4;
+            this.valaszok = new List<string> { valasz1, valasz2, valasz3, valasz4 };
             this.helyesValasz = helyesValasz;
             this.kategoria = kategoria;
         }
